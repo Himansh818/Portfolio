831769e7f1f36663e26b54c2f2a3f4645c73fc61
Himansh <footballerhimansh818@gmail.com>
2025-11-10 20:51:36 +0530

    Update Projects

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -3,8 +3,20 @@ import React from 'react'
 
 const Project = () => {
   return (
-    <div className='projects h-auto  w-full text-2xl flex justify-center items-center text-black '>
-      <div className='projectsDiv  flex flex-wrap py-10 gap-10 w-auto justify-around relative top-40 px-10'>
+    <div className=' projects h-auto  w-full text-2xl flex flex-col justify-center items-center text-black '>
+
+        <section className=' h-1/2 relative top-30 w-full flex flex-col gap-6 text-2xl text-white  '>
+          <h1 className='text-7xl text-center font-extrabold '>Projects</h1> 
+          <ul className="flex gap-3 w-fit pl-4 pr-4 ml-[10vw] rounded">
+            <li><a className='font-bold text-sm hover:text-blue-500 cursor-pointer' href="">All</a></li>
+            <li><a className='font-bold text-sm hover:text-blue-500 cursor-pointer' href="">AI</a></li>
+            <li><a className='font-bold text-sm hover:text-blue-500 cursor-pointer' href="">E-commerce</a></li>
+            <li><a className='font-bold text-sm hover:text-blue-500 cursor-pointer' href="">Game</a></li>
+          </ul>
+        </section>
+     
+     
+     <div className='projectsDiv  flex flex-wrap py-10 gap-10 w-auto justify-around relative top-40 px-10'>
 
         <div 
         onClick={() => window.open('https://himansh818.github.io/Tic-Tac-Toe-Game/', '_blank')}
