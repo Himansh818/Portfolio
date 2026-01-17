652bb689272b5ae5ca9b3d52e907ee682c7d7301
Himansh <footballerhimansh818@gmail.com>
2026-01-17 19:36:23 +0530

    Updated Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,25 +1,24 @@
-import React from "react";
 import "../ComponentsCss/Navbar.css";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="navbar mt-7 px-10 w-full rounded-2xl fixed h-10 flex justify-around items-center text-white gap-10">
+    <div className="navbar bg-white/15 backdrop-blur-md shadow-md px-10 w-full rounded-2xl fixed flex justify-around items-center text-white gap-10">
       <div className="col-1">
         <h2 className="text-white text-3xl font-extrabold">HPortfolio</h2>
       </div>
       <div className="col-2 flex gap-5 items-center font-bold">
-        <Link className="hover:text-cyan-600" to="/">
+        <Link className="link hover:text-cyan-600" to="/">
           Home
         </Link>
-        <Link className="hover:text-cyan-600" to="/projects">
+        <Link className="link hover:text-cyan-600" to="/projects">
           Projects
         </Link>
-        <Link className="h-10 w-10 rounded-full hover:bg-white flex justify-center items-center bg-white/20 backdrop-blur-md shadow-md" to="/projects">
-          <img src="" alt="" className="" />
+        <Link className="link h-10 w-10 rounded-full hover:bg-white flex justify-center items-center bg-white/20 backdrop-blur-md shadow-md" to="/projects">
+          <img src="/logo.jpg" alt="image" className="bg-cover" />
         </Link>
         
-        <Link className="rounded-2xl p-5 bg-white/20 backdrop-blur-md shadow-md hover:bg-white hover:text-cyan-600" to="/projects">
+        <Link className="connectBtn rounded-2xl p-5 bg-white/20 backdrop-blur-md shadow-md hover:bg-white hover:text-cyan-600" to="/projects">
           Let's connect 
         </Link>
       </div>
